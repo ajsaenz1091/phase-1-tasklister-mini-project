615871a17ec47c296de778fd2684c61b4030e061
Alberto <ajsaenz1091@gmail.com>
2021-05-05 23:26:30 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const input = document.getElementById('new-task-description'); // input field
+  const submit = input.nextElementSibling; // submit button
+  const ul = document.getElementById('tasks'); // Unordered List
+
+
+  submit.addEventListener('click', createNewTask)
+
+  
+  const createNewTask = (e) => {
+    const li = document.createElement('li') // <li>  </li>
+    const Item = input.value;
+    li.innerHTML = Item;
+    ul.appendChild(li);
+    e.preventDefault();
+  }
+
+
 });
